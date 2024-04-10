d5480cf2490f10479e9d153d43552040df7a19d9
DamirBogdan39 <damirbogdan39@gmail.com>
2024-04-10 16:09:57 +0200

    Reformatted project structure

--- src/vectorstore.py
+++ src/vectorstore.py
@@ -16,6 +16,7 @@ def create_collection():
     A function that creates the collection. 
     Connects to milvus and creates fields, collection and indicies for vector field.
     """
+    print("Creating collection...")
     client = MilvusClient(
         uri="http://localhost:19530"
     )
@@ -32,23 +33,25 @@ def create_collection():
         name="bge_embeddings", dtype=DataType.FLOAT_VECTOR, dim=1024)
     splade_embeddings = FieldSchema(
         name="splade_embeddings", dtype=DataType.FLOAT_VECTOR, dim=30522)
-    source = FieldSchema(name="source", dtype=DataType.VARCHAR, max_length=500)
     page_content = FieldSchema(
         name="page_content", dtype=DataType.VARCHAR, max_length=3000)
-    title = FieldSchema(name="title", dtype=DataType.VARCHAR, max_length=500)
-    description = FieldSchema(
-        name="description", dtype=DataType.VARCHAR, max_length=500)
-    view_count = FieldSchema(name="view_count", dtype=DataType.INT64)
-    thumbnail_url = FieldSchema(
-        name="thumbnail_url", dtype=DataType.VARCHAR, max_length=500)
-    publish_date = FieldSchema(
-        name="publish_date", dtype=DataType.VARCHAR, max_length=500)
-    length = FieldSchema(name="length", dtype=DataType.VARCHAR, max_length=500)
-    author = FieldSchema(name="author", dtype=DataType.VARCHAR, max_length=500)
+    # source = FieldSchema(name="source", dtype=DataType.VARCHAR, max_length=500)
+    # title = FieldSchema(name="title", dtype=DataType.VARCHAR, max_length=500)
+    # description = FieldSchema(
+    #     name="description", dtype=DataType.VARCHAR, max_length=500)
+    # view_count = FieldSchema(name="view_count", dtype=DataType.INT64)
+    # thumbnail_url = FieldSchema(
+    #     name="thumbnail_url", dtype=DataType.VARCHAR, max_length=500)
+    # publish_date = FieldSchema(
+    #     name="publish_date", dtype=DataType.VARCHAR, max_length=500)
+    # length = FieldSchema(name="length", dtype=DataType.VARCHAR, max_length=500)
+    # author = FieldSchema(name="author", dtype=DataType.VARCHAR, max_length=500)
 
     # Define collection schema
-    schema = CollectionSchema(fields=[pk, bge_embeddings, splade_embeddings, source, page_content, title, description,
-                                      view_count, thumbnail_url, publish_date, length, author])
+    schema = CollectionSchema(
+        fields=[pk, bge_embeddings, splade_embeddings, page_content])
+    #   source, title, description,
+    #   view_count, thumbnail_url, publish_date, length, author])
 
     # Create collection
     collection_name = "huberman_rag"
@@ -78,39 +81,34 @@ def create_collection():
         collection_name="huberman_rag",
         index_params=index_params
     )
+    print("Collection created successfully.")
 
 
 def jsonize_document(doc: Document) -> dict:
-
     page_content = doc.page_content
-    source = doc.metadata["source"]
-    title = doc.metadata["title"]
-    description = doc.metadata["description"]
-    view_count = doc.metadata["view_count"]
-    thumbnail_url = doc.metadata["thumbnail_url"]
-    publish_date = doc.metadata["publish_date"]
-    length = doc.metadata["length"]
-    author = doc.metadata["author"]
+    # source = doc.metadata["source"]
+    # title = doc.metadata["title"]
+    # description = doc.metadata["description"]
+    # view_count = doc.metadata["view_count"]
+    # thumbnail_url = doc.metadata["thumbnail_url"]
+    # publish_date = doc.metadata["publish_date"]
+    # length = doc.metadata["length"]
+    # author = doc.metadata["author"]
 
     splade_emb = embed_splade(page_content)
     bge_emb = bge_embeddings.embed_query(page_content)
 
     json_doc = {
         "page_content": page_content,
-        "source": source,
-        "title": title,
-        "description": description,
-        "view_count": view_count,
-        "thumbnail_url": thumbnail_url,
-        "publish_date": publish_date,
-        "length": length,
-        "author": author,
+        # "source": source,
+        # "title": title,
+        # "description": description,
+        # "view_count": view_count,
+        # "thumbnail_url": thumbnail_url,
+        # "publish_date": publish_date,
+        # "length": length,
+        # "author": author,
         "splade_embeddings": splade_emb,
         "bge_embeddings": bge_emb
     }
-
     return json_doc
-
-
-if __name__ == "__main___":
-    create_collection()
